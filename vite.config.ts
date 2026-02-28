f58b33ae6f4bee54b2d9e20dea8f356c98e4e48c
nvimcraft <260064684+nvimcraft@users.noreply.github.com>
2026-02-28 17:07:09 -0500

    chore: establish dev server and alias baseline

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -6,8 +7,24 @@ export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
+        plugins: ['babel-plugin-react-compiler']
+      }
+    })
   ],
+
+  base: '/',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+
+  server: {
+    port: 3000,
+    strictPort: true,
+    host: true, // listen on all addresses (LAN/public)
+    hmr: {
+      port: 3000
+    }
+  }
 })
